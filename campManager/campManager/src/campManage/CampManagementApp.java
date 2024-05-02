cf393f75b56b29cb917d067b8fa8953f7d92771d
minnn242 <165253760+minnn242@users.noreply.github.com>
2024-05-02 16:42:52 +0900

    test text

--- campManager/campManager/src/campManage/CampManagementApp.java
+++ campManager/campManager/src/campManage/CampManagementApp.java
@@ -1,5 +1,13 @@
 package campManage;
 
 public class CampManagementApp {
-
+    public static void main(String[] args) throws  CampManagementApp  {
+        Scanner scanner = new Scanner(new File("C:\\Users\\민규\\IdeaProjects\\SpartaCampManagementApp\\campManager\\campManager\\src\\campManage\\src\\Database.txt");
+        while (scanner.hasNext()) {
+            String str = scanner.next();
+            System.out.println(str);
+        }
+    }
 }
+출처:https://hianna.tistory.com/587 [어제 오늘 내일:티스토리]
+        }
--- campManager/campManager/src/campManage/scoreManagement/ScoreManagement_KMG.java
+++ campManager/campManager/src/campManage/scoreManagement/ScoreManagement_KMG.java
@@ -1,4 +1,13 @@
 package campManage.scoreManagement;
 
-public class ScoreManagement_KMG {
+public class scoreManagement_KMG.java {
+    public static void main(String[] args) throws  scoreManagement_KMG.java  {
+        Scanner scanner = new Scanner(new File("C:\\Users\\민규\\IdeaProjects\\SpartaCampManagementApp\\campManager\\campManager\\src\\campManage\\src\\Database.txt");
+        while (scanner.hasNext()) {
+            String str = scanner.next();
+            System.out.println(str);
+        }
+    }
 }
+
+        }
