06dd8daf6e92a3c29ad5f503365ea6fe2a6add9c
minnn242 <165253760+minnn242@users.noreply.github.com>
2024-05-03 09:03:02 +0900

    test text

--- campManager/campManager/src/campManage/scoreManagement/ScoreManagement_KMG.java
+++ campManager/campManager/src/campManage/scoreManagement/ScoreManagement_KMG.java
@@ -1,8 +1,11 @@
 package campManage.scoreManagement;
 
-public class scoreManagement_KMG.java {
-    public static void main(String[] args) throws  scoreManagement_KMG.java  {
-        Scanner scanner = new Scanner(new File("C:\\Users\\민규\\IdeaProjects\\SpartaCampManagementApp\\campManager\\campManager\\src\\campManage\\src\\Database.txt");
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class ScoreManagement_KMG {
+    public static void run() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("C:\\Users\\민규\\IdeaProjects\\SpartaCampManagementApp\\campManager\\campManager\\src\\campManage\\src\\Database.txt"));
         while (scanner.hasNext()) {
             String str = scanner.next();
             System.out.println(str);
