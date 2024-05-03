6c7b31fdcbd8086a35f81caf42f8c7155007ec58
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-05-03 13:22:49 +0900

    DB JSON 연결, 기본틀 무한루프 생성
    - JSON 라이브러리 연결
    - StudentManagementApp 무한루프 적용

--- campManager/campManager/src/campManage/CampManagementApp.java
+++ campManager/campManager/src/campManage/CampManagementApp.java
@@ -18,24 +18,32 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class CampManagementApp {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        System.out.println("========================================");
-        System.out.println("= = = 실행할 프로젝트를 선택해주세요 = = =");
-        System.out.println("1. 학생관리 _ 김도훈");
-        System.out.println("2. 학생관리 _ 이재성");
-        System.out.println("3. 학생관리 _ 백원하");
-        System.out.println("4. 점수관리 _ 최유진");
-        System.out.println("5. 점수관리 _ 김민규");
-        System.out.println("6. 점수관리 _ 백원하");
-        switch (sc.nextLine().charAt(0)){
-            case '1' -> StudentManagement_KDH.run();
-            case '2' -> StudentManagement_LJS.run();
-            case '3' -> StudentManagement_BWH.run();
-            case '4' -> ScoreManagement_CYJ.run();
-            case '5' -> ScoreManagement_KMG.run();
-            case '6' -> ScoreManagement_BWH.run();
-            default -> System.out.println("재실행");
+        loop:
+        while (true) {
+            System.out.println("========================================");
+            System.out.println("= = = 실행할 프로젝트를 선택해주세요 = = =");
+            System.out.println("1. 학생관리 _ 김도훈");
+            System.out.println("2. 학생관리 _ 이재성");
+            System.out.println("3. 학생관리 _ 백원하");
+            System.out.println("4. 점수관리 _ 최유진");
+            System.out.println("5. 점수관리 _ 김민규");
+            System.out.println("6. 점수관리 _ 백원하");
+            System.out.println("0. 프로그램 종료");
+            switch (sc.nextLine()) {
+                case "1" -> StudentManagement_KDH.run();
+                case "2" -> StudentManagement_LJS.run();
+                case "3" -> StudentManagement_BWH.run();
+                case "4" -> ScoreManagement_CYJ.run();
+                case "5" -> ScoreManagement_KMG.run();
+                case "6" -> ScoreManagement_BWH.run();
+                case "0" -> {
+                    System.out.println("= = = 프로그램을 종료합니다 = = =");
+                    break loop;
+                }
+                default -> System.out.println("재실행");
+            }
         }
     }
 }
---
+++ campManager/campManager/src/campManage/src/DBConfig.java
@@ -0,0 +1,6 @@
+package campManage.src;
+
+public class DBConfig {
+    public static final String PATH = "campManager/campManager/src/campManage/src/Database.txt";
+    public static final String PATH_BWH = "campManager/campManager/src/campManage/src/Database_백원하.txt";
+}
